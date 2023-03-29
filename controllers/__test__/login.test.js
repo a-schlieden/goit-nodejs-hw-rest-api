7aa423e809b4a1b3d2363d2b7e56edd0fda3fb43
Alexander <alex.schlieden@gmail.com>
2023-03-29 13:04:34 +0200

    test user login beta 3

--- controllers/__test__/login.test.js
+++ controllers/__test__/login.test.js
@@ -1,16 +1,74 @@
 
+// const request = require("supertest");
+// const app = require("../../app");
+
+// describe("test login controller", () => {
+//   beforeAll(() => app.listen(3000));
+
+//   test("Testing status, Object user und token ", async () => {
+//     const data = {
+//       email: "user@example.com",
+//       password: "123456",
+//     };
+//     const responce = await request(app).post("/api/auth/login").send(data);
+//     console.log("Status ", responce.status);
+//   }, 3000);
+// });
+
+
+
+/////////
+
+
+const express = require("express");
+const mongoose = require("mongoose");
 const request = require("supertest");
-const app = require("../../app");
+require("dotenv").config();
 
-describe("test login controller", () => {
-  beforeAll(() => app.listen(3000));
+const login = require("../auth/login")
 
-  test("Testing status, Object user und token ", async () => {
-    const data = {
-      email: "user@example.com",
-      password: "123456",
-    };
-    const responce = await request(app).post("/api/auth/login").send(data);
-    console.log("Status ", responce.status);
-  }, 3000);
+const { DB_HOST } = process.env;
+
+const connect = async () => {
+  mongoose.set("strictQuery", false);
+  return mongoose.connect(DB_HOST);
+};
+
+const app = express();
+
+app.use(express.json());
+
+app.post("/api/auth/login", login);
+
+describe("controller login test", () => {
+  beforeAll(async () => {
+    await connect()
+      .then(async () => {
+        console.log("Database connection successful");
+        app.listen(3001);
+      })
+      .catch((error) => {
+        console.log(`Database connection with error:${error.message}`);
+      });
+  });
+
+  const data = {
+    email: "user@example.com",
+    password: "123456",
+  };
+
+  test("login return token and object with user und subscription", async () => {
+    const response = await request(app).post("/api/users/login").send(data);
+
+    const { email, subscription, avatarURL } = response.body.user;
+    const { token } = response.body;
+    console.log("mail ", email);
+    // expect(response.status).toBe(200);
+    // expect(typeof token).toEqual("string");
+    // expect(Object.keys(response.body.user).length).toBe(3);
+    // expect(typeof email).toEqual("string");
+    // expect(typeof subscription).toEqual("string");
+    // expect(typeof avatarURL).toEqual("string");
+  });
+
 });
--- controllers/user/updAvatar.js
+++ controllers/user/updAvatar.js
@@ -1,6 +1,8 @@
 const { User } = require("../../models/user");
 const path = require("path");
 const fs = require("fs").promises;
+var jimp = require("jimp");
+
 
 const imagesDir = path.join(__dirname, "../../", "publik", "avatars");
 
@@ -8,8 +10,18 @@ const updateAvatar = async (req, res, next) => {
   const { path: tempUpload, originalname } = req.file;
   const { _id } = req.user;
   const imgName = `${_id}-${originalname}`;
+
+
+  const residedImg = await jimp.read("imgName", (err, imgName) => {
+    if (err) throw err;
+    imgName.resize(250, 250)
+  });
+
+
+
   try {
-    const resultUpload = path.join(imagesDir, imgName);
+    const resultUpload = path.join(imagesDir, residedImg);
+    //const resultUpload = path.join(imagesDir, imgName);
     await fs.rename(tempUpload, resultUpload);
     const avatarUrl = path.join("publik", "avatars", imgName);
     await User.findByIdAndUpdate(req.user._id, { avatarUrl });
