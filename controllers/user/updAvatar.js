60227e55347b4b18c8b5211d2a8fc77cfc6db9a1
Alexander <alex.schlieden@gmail.com>
2023-03-29 13:10:00 +0200

    add image resize in file updAvatar.js

--- controllers/user/updAvatar.js
+++ controllers/user/updAvatar.js
@@ -7,23 +7,29 @@ var jimp = require("jimp");
 const imagesDir = path.join(__dirname, "../../", "publik", "avatars");
 
 const updateAvatar = async (req, res, next) => {
+
   const { path: tempUpload, originalname } = req.file;
   const { _id } = req.user;
-  const imgName = `${_id}-${originalname}`;
+
+  const extention = originalname.split(".").pop();
 
 
-  const residedImg = await jimp.read("imgName", (err, imgName) => {
-    if (err) throw err;
-    imgName.resize(250, 250)
-  });
-
-
+  const imgName = `${_id}-${originalname}.${extention}`;
 
   try {
-    const resultUpload = path.join(imagesDir, residedImg);
-    //const resultUpload = path.join(imagesDir, imgName);
+    const resultUpload = path.join(imagesDir, imgName);
     await fs.rename(tempUpload, resultUpload);
     const avatarUrl = path.join("publik", "avatars", imgName);
+
+
+    jimp.read(resultUpload)
+      .then((image) => {
+        return image.resize(250, 250).write(resultUpload);
+      })
+      .catch((err) => {
+        err.message = "Can't change image size or name";
+      });
+
     await User.findByIdAndUpdate(req.user._id, { avatarUrl });
     res.json({ avatarUrl });
   } catch (error) {
